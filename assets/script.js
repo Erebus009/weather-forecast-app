97e4cfac027f3c188d12984a6921a17a18a5cd29
Travis Puryear <travispuryear678@gmail.com>
2021-10-02 12:22:15 -0400

    GetCityweather function to take in city searched and fetch data for that city.

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,75 @@
-var previousSearchEl = document.querySelector('.previous')
-var searchBtn = document.querySelector('.searchBtn');
+var previousSearchEl = document.querySelector(".previous");
+var searchBtn = document.querySelector(".searchBtn");
+var cityWeatherEL = document.querySelector(".weather-info");
+var fiveDayForecast = document.querySelector(".card-weather");
+var inputEL = document.querySelector('#city-input')
+
+// var url ="https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&limit=5&appid=0bc4f134f7113eaee48d311d5efa7749";
 
 
 
 
+// handles the input submit from search for city.
+var submitHandler = function (event) {
+    event.preventDefault();
 
+    var city = inputEL.value.trim();
+    
+    if (city){
+        inputEL.textContent='';
+        inputEL.value='';
+        appendLastCity(city);
+        displayCityWeather(city);
+        
+    } 
+    
+};
 
-
-
+// create button of last user input.
 var appendLastCity = function (city) {
 
-    console.log('test');
+  console.log("test");
 
-city = document.getElementById('city-input').value;
+  
+
+    var parent = document.querySelector(".previous");
+
+    var lastCity = document.createElement("button");
+    lastCity.classList = "col-12 text-center text-light btn btn-secondary column mt-3 mb-3";
+    lastCity.textContent = city;
+
+    parent.appendChild(lastCity);
+
+    document.getElementById("city-input").textContent = null;
+
+    return;
+  }
 
 
-var parent = document.querySelector('.previous');
-
-
-var lastCity = document.createElement('button');
-lastCity.classList="col-12 text-center text-light btn btn-secondary column mt-3 mb-3";
-lastCity.textContent= city;
-
-
-parent.appendChild(lastCity)
-
-
-
-
+ var getCityWeather = function(city) {
+    
+    var url ="https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&limit=5&appid=0bc4f134f7113eaee48d311d5efa7749";
+    
+    console.log(url);
+    fetch(url)
+    .then(function (response) {
+      if (response.status === 200) {
+        response.json().then(function(data) {
+            displayCityWeather(data,city);
+            
+        });
+    }else{
+        alert('Error ' + response.statusText);
+      }
+      
+    })
+    
+    
+  return;
+    
+        
 }
 
-searchBtn.addEventListener('click', appendLastCity);
+searchBtn.addEventListener("click", submitHandler);
+
+
