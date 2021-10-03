4ee22b761be315cabc84fd45a397a6886879f348
Travis Puryear <travispuryear678@gmail.com>
2021-10-03 19:04:58 -0400

    Forecast js added that icons fit the correct forecasted day's weather.

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var searchBtn = document.querySelector(".searchBtn");
 var cityWeatherEL = document.querySelector(".weather-info");
 var fiveDayForecast = document.querySelector(".card-weather");
 var inputEL = document.querySelector('#city-input')
-
+var grayButton = document.querySelector('.grayButton')
 
 // var url ="https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us&limit=5&appid=0bc4f134f7113eaee48d311d5efa7749";
 
@@ -23,7 +23,7 @@ var submitHandler = function (event) {
       var Day3 = document.querySelector('.card-body-day3');
       var Day4 = document.querySelector('.card-body-day4');
       var Day5 = document.querySelector('.card-body-day5');
-        var CityEL = document.querySelector('.weather-info')
+      var CityEL = document.querySelector('.weather-info')
         forecast.innerHTML='';
         Day1.innerHTML='';
         Day2.innerHTML='';
@@ -45,7 +45,7 @@ var appendLastCity = function (city) {
 
   console.log("test");
 
-    
+ 
 
 
     var parent = document.querySelector(".previous");
@@ -53,11 +53,11 @@ var appendLastCity = function (city) {
     var lastCity = document.createElement("button");
     lastCity.classList = "col-12 text-center text-light btn btn-secondary column mt-3 mb-3";
     lastCity.textContent = city;
+    lastCity.classList.add('grayButton')
 
     parent.appendChild(lastCity);
 
     document.getElementById("city-input").textContent = null;
-
     return;
   }
 
@@ -142,10 +142,17 @@ var displayCurrent = function (data, city) {
 
   // refrence to get icon from openweater icon list and append to created elements. 
   var iconcode = data.current.weather[0].icon;
+  var iconcode1 = data.daily[0].weather[0].icon;
+  var iconcode2 = data.daily[1].weather[0].icon;
+  var iconcode3 = data.daily[2].weather[0].icon;
+  var iconcode4 = data.daily[3].weather[0].icon;
+  var iconcode5 = data.daily[4].weather[0].icon;
   var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-  var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
-  img.attr('src', iconurl);
-  img.appendTo('.icon');
+  var iconurl1 = "http://openweathermap.org/img/w/" + iconcode1 + ".png";
+  var iconurl2 = "http://openweathermap.org/img/w/" + iconcode2 + ".png";
+  var iconurl3 = "http://openweathermap.org/img/w/" + iconcode3 + ".png";
+  var iconurl4 = "http://openweathermap.org/img/w/" + iconcode4 + ".png";
+  var iconurl5 = "http://openweathermap.org/img/w/" + iconcode5 + ".png";
   
   
 
@@ -156,7 +163,9 @@ var displayCurrent = function (data, city) {
     // day 1 forecast selectors. 
     var DatesEl =document.createElement('h4');
     DatesEl.textContent = moment().add(1, 'days').format('D/MM/YY')
-   
+    
+    var icon1= document.createElement('p')
+    icon1.classList.add('icon1');
 
     var TempEL2 =document.createElement('p')
     TempEL2.textContent = 'Temp : ' + data.current.temp + '°F';
@@ -169,6 +178,9 @@ var displayCurrent = function (data, city) {
 // Day 2 forecast card div box elements
     var DatesEl2 =document.createElement('h4');
     DatesEl2.textContent = moment().add(2,'days').format('D/MM/YY')
+
+    var icon2= document.createElement('p')
+    icon2.classList.add('icon2');
 
     var TempEL3 =document.createElement('p')
     TempEL3.textContent = 'Temp : ' + data.daily[1].temp.day + '°F';
@@ -184,6 +196,9 @@ var displayCurrent = function (data, city) {
     var DatesEl3 =document.createElement('h4');
     DatesEl3.textContent = moment().add(3,'days').format('D/MM/YY')
 
+    var icon3= document.createElement('p')
+    icon3.classList.add('icon3')
+
     var TempEL4 =document.createElement('p')
     TempEL4.textContent = 'Temp : ' + data.daily[2].temp.day + '°F';
     
@@ -198,6 +213,9 @@ var displayCurrent = function (data, city) {
     var DatesEl4 =document.createElement('h4');
     DatesEl4.textContent = moment().add(4,'days').format('D/MM/YY')
 
+    var icon4= document.createElement('p')
+    icon4.classList.add('icon4')
+
     var TempEL5 =document.createElement('p')
     TempEL5.textContent = 'Temp : ' + data.daily[3].temp.day + '°F';
     
@@ -210,6 +228,9 @@ var displayCurrent = function (data, city) {
 
 var DatesEl5 =document.createElement('h4');
 DatesEl5.textContent = moment().add(5,'days').format('D/MM/YY')
+
+var icon5= document.createElement('p')
+    icon5.classList.add('icon5')
 
 var TempEL6 =document.createElement('p')
 TempEL6.textContent = 'Temp : ' + data.daily[4].temp.day + '°F';
@@ -268,34 +289,63 @@ Day5.classList.add('forecast');
     CityEL.appendChild(UVindexEL).appendChild(UVindexELColor)
     // Day 1 forecast appnedchild
     Day1.appendChild(DatesEl)
+    Day1.appendChild(icon1);
     Day1.appendChild(TempEL2);
     Day1.appendChild(WindSpeedEL2);
     Day1.appendChild(HumidityEL2);
+    
     // day 2 forecast
     Day2.appendChild(DatesEl2)
+    Day2.appendChild(icon2);
     Day2.appendChild(TempEL3);
     Day2.appendChild(WindSpeedEL3);
     Day2.appendChild(HumidityEL3);
    // day 3 forecast`
     Day3.appendChild(DatesEl3)
+    Day3.appendChild(icon3);
     Day3.appendChild(TempEL4);
     Day3.appendChild(WindSpeedEL4);
     Day3.appendChild(HumidityEL4);
     // day 4 forecast
     Day4.appendChild(DatesEl4)
+    Day4.appendChild(icon4);
     Day4.appendChild(TempEL5);
     Day4.appendChild(WindSpeedEL5);
     Day4.appendChild(HumidityEL5);
     // day 5 forecast
     Day5.appendChild(DatesEl5)
+    Day5.appendChild(icon5);
     Day5.appendChild(TempEL6);
     Day5.appendChild(WindSpeedEL6);
     Day5.appendChild(HumidityEL6);
 
-    
+    var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+    img.attr('src', iconurl);
+    img.appendTo('.icon');
+
+    var img1 = $('<img id="dynamic">'); 
+    img1.attr('src', iconurl1);
+    img1.appendTo('.icon1');
+
+    var img2 = $('<img id="dynamic">'); 
+    img2.attr('src', iconurl2);
+    img2.appendTo('.icon2');
+
+    var img3 = $('<img id="dynamic">'); 
+    img3.attr('src', iconurl3);
+    img3.appendTo('.icon3');
+
+    var img4 = $('<img id="dynamic">'); 
+    img4.attr('src', iconurl4);
+    img4.appendTo('.icon4');
+
+    var img5 = $('<img id="dynamic">'); 
+    img5.attr('src', iconurl5);
+    img5.appendTo('.icon5');
+
+
 
 }
-
 
 // Search when button is clicked runs submit handler to gather api data about what city was searched. 
 searchBtn.addEventListener("click", submitHandler);
